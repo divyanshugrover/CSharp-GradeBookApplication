bd217426797d142d4c3f33737ab11c82bff83075
Divyanshu Grover <digrove@microsoft.com>
2019-01-17 19:59:37 +0530

    CH3 changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,11 +5,36 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            List<double> studentGrades = new List<double>();
+            Students.ForEach(s => studentGrades.Add(s.AverageGrade));
+            studentGrades.Sort();
+            var index = studentGrades.FindIndex(val => val >= averageGrade);
+            int bucket = (index * 5) / studentGrades.Count;
+            switch (bucket)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+            }
+            return 'F';
         }
     }
 }
